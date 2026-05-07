8df949da8d75d55dddd93c18bff434a09ab8991e
cjlrestlong-AI <cjlstephenchan@gmail.com>
2026-05-08 05:19:14 +0800

    更新SW快取版本v2，強制重新加載新JS

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Provides offline support & caching
    ============================================ */
 
-const CACHE_NAME = 'naval-books-v1';
+const CACHE_NAME = 'naval-books-v2';
 const ASSETS_TO_CACHE = [
   '.',
   'index.html',
